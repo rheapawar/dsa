55cfb31aef020f0a8eedc4b6c0fdc5246003fb46
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-05-14 23:43:39 -0400

    Time: 0 ms (100.00%) | Memory: 21.7 MB (81.03%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -2,39 +2,37 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         //validating each row
+        vector<bool> count(10);
         for(int i = 0; i < board.size(); i++){
-            vector<int> count(10);
-            iota(count.begin(), count.end(), 0);
+            count.assign(10, false);
             for(int j = 0; j < board[i].size(); j++){
                 char c = board[i][j];
                 if(c != '.'){
-                    if (count[c - '0'] != c - '0') return false;
-                    count[c - '0'] = -1;
+                    if (count[c - '0']) return false;
+                    count[c - '0'] = true;
                 }
             }
         }
         //validating columns
         for(int i = 0; i < 9; i++){
-            vector<int> count(10);
-            iota(count.begin(), count.end(), 0);
+            count.assign(10, false);
             for(int j = 0; j < 9; j++){
                 char c = board[j][i];
                 if(c != '.'){
-                    if (count[c - '0'] != c - '0') return false;
-                    count[c - '0'] = -1;
+                    if (count[c - '0']) return false;
+                    count[c - '0'] = true;
                 }
             }
         }
         //validating subboxes
         for(int i = 0; i < 9; i++){
-            vector<int> count(10);
-            iota(count.begin(), count.end(), 0);
+            count.assign(10, false);
             for(int j = 0; j < 3; j++){
                 for(int k = 0; k < 3; k++){
                     char c = board[3*(i/3) + j][3*(i%3) + k];
                     if(c != '.'){
-                        if (count[c - '0'] != c - '0') return false;
-                        count[c - '0'] = -1;
+                        if (count[c - '0']) return false;
+                        count[c - '0'] = true;
                     }
                 } 
             }
