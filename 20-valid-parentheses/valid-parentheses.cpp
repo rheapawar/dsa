eac17394fcb788eb4b81f399ea40889ead2ab160
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-05-17 23:11:43 -0400

    Time: 0 ms (100.00%) | Memory: 9 MB (19.53%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> check;
+        for(char a : s){
+            if( a =='[' || a == '(' || a == '{'){
+                check.push(a);
+            }
+            else{
+                if(check.empty()) return false;
+                char b = check.top();
+                check.pop();
+                if(a == ')' && b != '(') return false;
+                if(a == '}' && b != '{') return false;
+                if(a == ']' && b != '[') return false;
+        }
+        }
+        return check.empty();
+    }
+};
