79198d9f1ef5d950fbb9d2d8fd1a8fca47337c7e
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-05-31 19:50:45 -0400

    Time: 2 ms (81.41%) | Memory: 12.9 MB (89.11%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,19 +2,28 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> perms;
-        helper(n,0, "", perms);
+        string perm;
+        helper(n,0, perm, perms);
         return perms;
     }
 
-    void helper(int o, int c, string perm, vector<string> &res){
+    void helper(int o, int c, string &perm, vector<string> &res){
         if(o == 0 && c == 0){
             res.push_back(perm);
             return;
         }
         else{
-            if (o >= 0) helper(o-1, c+1, perm + "(", res);
-            if (c > 0) helper(o, c-1, perm + ")", res);   
+            if (o >= 0){
+                perm.push_back('(');
+                helper(o-1, c+1, perm, res);
+                perm.pop_back();
+            }
+            if (c > 0){
+                perm.push_back(')');
+                helper(o, c-1, perm, res);   
+                perm.pop_back();
             }
         }
+    }
     
 };
