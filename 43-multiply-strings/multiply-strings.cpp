9b35093a1e123cf2587e1f15a70ac9bc97d71d02
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-06-16 23:48:22 -0400

    Time: 0 ms (100.00%) | Memory: 9.5 MB (88.44%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1 == "0" || num2 == "0") return "0";
+        vector<int> res(num1.length() + num2.length(), 0);
+        for(int i = num1.length()-1; i >= 0; i--){
+            for(int j = num2.length()-1; j>= 0; j--){
+                res[i+j+1] += (num1[i] - '0')*(num2[j] - '0') ;
+                res[i+j] += res[i+j+1]/10;
+                res[i+j+1] %= 10;
+            } 
+        }
+
+        string ans = "";
+        int i = 0; 
+        while(i < res.size() && res[i] == 0) ++i;
+        while(i < res.size()) ans.push_back(res[i++] + '0');
+
+        return ans;
+    }
+};
