e43143d0b09d615fad9e48adac610fe93dad1b0f
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-06-17 23:30:47 -0400

    Time: 0 ms (100.00%) | Memory: 15.3 MB (8.79%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,14 +4,16 @@ public:
         unordered_map<int,int> m;
         vector<int> ans;
         ans.reserve(2);
+        m.reserve(nums.size());
         for(int i = 0; i < nums.size(); i++){
-            auto it = m.find(target-nums[i]);
+            int x = nums[i];
+            auto it = m.find(target-x);
             if(it != m.end()){
                 ans.push_back(i);
                 ans.push_back(it->second);
                 break;
             }
-            else m[nums[i]] = i;
+            else m[x] = i;
         }
         return ans;
     }
