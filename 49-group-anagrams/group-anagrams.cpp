4ccf17e4ecca144b9798d2e8a99b8a03d480de06
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-08-05 15:33:04 -0400

    Time: 24 ms (25.30%) | Memory: 25.5 MB (60.87%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -6,13 +6,28 @@ public:
         unordered_map<string, vector<string>> map;
         map.reserve(strs.size());
         for(const auto &x : strs){
-            string a = x;
-            sort(a.begin(), a.end());
-            map[a].push_back(x);
+            map[getKey(x)].push_back(x);
+
         }
         for(auto it = map.begin(); it != map.end(); it++){
             res.push_back(it->second);
         }
         return res;
+    }
+
+    string getKey(string s){
+        int counts[26] = {};
+        for(char c : s){
+            counts[c - 'a']++;
+        }
+
+        string a = "";
+        for(int i = 0; i < 26; i++){
+            if(counts[i] > 0){
+                a += char('a' + i);
+                a += to_string(counts[i]);
+            }
+        }
+        return a;
     }
 };
