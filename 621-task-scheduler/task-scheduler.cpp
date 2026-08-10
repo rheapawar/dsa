9edc7dc6cb0dfd1624629373376d49e7048e6ca4
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-08-10 12:31:34 -0400

    Time: 0 ms (100.00%) | Memory: 38.4 MB (70.72%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -2,39 +2,16 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         int intervals = 0;
-        vector<int> counts(26);
-        vector<int> pq;
-        pq.reserve(26);
+        vector<int> counts(26, 0);
+        int max_freq = 0;
         for(char c : tasks){
-            counts[c -'A']++;
+            max_freq = max(max_freq, ++counts[c - 'A']);
         }
+        int num_max = 0;
         for(int c : counts){
-            if(c > 0) pq.push_back(c);
+            if(c == max_freq) num_max++;
         }
-        sort(pq.begin(), pq.end());
-
-        while(!pq.empty()){
-            int i = 0;
-            vector<int> q;
-            while(i < n+1 && !pq.empty()){
-                int x = pq.back() - 1;
-                pq.pop_back();
-                if(x > 0) q.push_back(x);
-                intervals++;
-                i++;
-            }
-            while(!q.empty()){
-                pq.push_back(q.back());
-                q.pop_back();
-            }
-            if(!pq.empty()){
-                intervals += n - i + 1;
-                sort(pq.begin(), pq.end());
-            }
-        }
-        return intervals;
+        intervals = (max_freq - 1)*(n+1) + num_max;
+        return max(intervals, (int)tasks.size());
     }
-
-    
-
 };
