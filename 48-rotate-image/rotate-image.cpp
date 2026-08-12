e60e62d7764f2a398ea222090120006fa5b10931
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-08-12 15:34:05 -0400

    Time: 0 ms (100.00%) | Memory: 10.1 MB (93.99%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -14,14 +14,16 @@ public:
         do like a dfs
 
         */
+        
         int n = matrix.size();
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                swap(matrix[i][j], matrix[j][i]);
+        for(int i = 0; i < n/2; i++){
+            for(int j = i; j < n - i - 1; j++){
+                int top_left = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = top_left;
             }
-        }
-        for(int i = 0; i < matrix.size(); i++){
-            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
