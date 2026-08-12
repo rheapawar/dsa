8b1e0e394d9691f913f69371d90e833974de26f3
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-08-12 15:17:02 -0400

    Time: 0 ms (100.00%) | Memory: 10.3 MB (38.02%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        /* 
+        need to swap rows and columns essentially, first row and last column so n x n matrix
+        0th row and n-1 column swap and then reverse the col
+
+        or for four way swap cycle
+        to do this, iterate over the rows so matrix.size(), for each, need to swap 
+        but need to prevent overwriting data, so is it simply that every data point has to move 
+        new row = old col
+        new col = n - 1 - old row
+        how to know when you have finished, maybe total count of the number of tiles changed??
+        do like a dfs
+
+        */
+        int n = matrix.size();
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i = 0; i < matrix.size(); i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
