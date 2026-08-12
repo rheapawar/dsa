c1fbf80b343a3cfd90183eea366d7f6ec5f646f0
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-08-12 16:42:13 -0400

    Time: 0 ms (100.00%) | Memory: 9.3 MB (57.61%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        /*go across a row, then down the column, then reverse across the bottom most row and then up the col and then right until bounds infringe on each other
-        
-        essentially use while loop, i for rows, j for cols
-        for i = 0, go from 0 to n
-        then once u reach bound, go down the outer from i to m, move m to subtract m and increase i
-
-        then go from k to 0 and after that, increase to j to 1 and decrease k
-
-        to determine direction, if i and j mod 2 is positive, then go in forward direction, else go in reverse
-        
-        */
         vector<int> res;
         int bottom = matrix.size();
         int right = matrix[0].size();
