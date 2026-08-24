f1092e91cb85a267bd100611465e6ee6e74a31f2
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-08-24 15:37:53 -0400

    Time: 0 ms (100.00%) | Memory: 14.3 MB (61.77%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -6,15 +6,16 @@ public:
         10,9,2,3,5,7,4,5
         issue is if you run into a number where the streak before doesnt include the actual pre value then u cant increment by one - so does dp instead hold the value of the largest streak containing that element and then a separate variable for max streak so far?
         */
-        vector<int> memo(nums.size(), 1);
-        int streak = 0;
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j < i; j++){
-                if(nums[i] > nums[j] && memo[i] < memo[j] + 1){
-                    memo[i] = memo[j] + 1;
-                }
+        vector<int> streak;
+        streak.reserve(nums.size());
+        for(int n : nums){
+            if(streak.empty() || streak.back() < n) streak.push_back(n);
+            else{
+                auto it = lower_bound(streak.begin(), streak.end(), n);
+                *it = n;
             }
         }
-        return *max_element(memo.begin(), memo.end());
-    }
+        return streak.size();
+        
+        }
 };
