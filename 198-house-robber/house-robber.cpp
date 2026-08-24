405261fb3c2e1c471650142a7faa1f976145e6a9
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-08-24 01:35:03 -0400

    Time: 0 ms (100.00%) | Memory: 10.1 MB (90.43%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,12 +4,13 @@ public:
         /*cant rob two adjacent houses and so need to determine the max amount of money of */
         if(nums.size() == 1) return nums[0];
         int n = nums.size();
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        for(int i = 2; i < n; i++){
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+        int p1 = 0;
+        int p2 = 0;
+        for(auto n : nums){
+            int tmp = p1;
+            p1 = max(n + p2, p1);
+            p2 = tmp;
         }
-        return max(dp[n-2], dp[n-1]);
+        return max(p1, p2);
     }
 };
