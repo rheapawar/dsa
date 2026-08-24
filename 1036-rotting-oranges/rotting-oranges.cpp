6e868d5685e2ea7d4dbbec46550c092a31556c35
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-08-23 22:00:42 -0400

    Time: 4 ms (13.45%) | Memory: 16.5 MB (99.74%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,47 +1,51 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        /*traverse over the grid once, any rotten orange add to the queue and then once added, go over queue while not empty - check all surroundings and if it is a 1, change to rotten and then add it to the queue - need a way to distinguish between the levels for res tho
+        
+        could store as another variable but could also use a counter to when the current level ends
+        */
+        int res = 0;
+        bool ones = false;
         queue<pair<int,int>> q;
-        int min = 0;
         for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); ++j){
+            for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j] == 2) q.push({i,j});
+                if(grid[i][j] == 1) ones = true;
             }
         }
+        if(!ones) return 0;
+        
         while(!q.empty()){
             int s = q.size();
             for(int i = 0; i < s; i++){
-                auto it = q.front();
+                int x = q.front().first;
+                int y = q.front().second;
                 q.pop();
-                int x = it.first;
-                int y = it.second;
                 if(x > 0 && grid[x-1][y] == 1){
                     grid[x-1][y] = 2;
                     q.push({x-1,y});
                 }
+                if(x < grid.size()-1 && grid[x+1][y] == 1){
+                    grid[x+1][y] = 2;
+                    q.push({x+1,y});
+                }
                 if(y > 0 && grid[x][y-1] == 1){
                     grid[x][y-1] = 2;
-                    q.push({x,y-1});
+                    q.push({x, y-1});
                 }
                 if(y < grid[0].size()-1 && grid[x][y+1] == 1){
                     grid[x][y+1] = 2;
-                    q.push({x,y+1});
+                    q.push({x, y+1});
                 }
-                if(x < grid.size() -1 && grid[x+1][y] == 1){
-                    grid[x+1][y] = 2;
-                    q.push({x+1,y}); 
-                }
-                
             }
-            if(q.size() > 0)min++;
+            if(q.size() > 0) res++;
         }
         for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); ++j){
+            for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j] == 1) return -1;
             }
         }
-        return min;
+       return res;
     }
-
-   
 };
