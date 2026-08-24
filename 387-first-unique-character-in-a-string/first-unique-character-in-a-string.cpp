6c3a5fdc4eacd467b80f2de897a7f6eb43ea59b4
rheapawar <89950010+rheapawar@users.noreply.github.com>
2026-08-24 13:38:34 -0400

    Time: 0 ms (100.00%) | Memory: 14.7 MB (72.94%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> map;
+        vector<int> map(26);
         for(char c : s){
-            map[c]++;
+            map[c - 'a']++;
         }
         for(int i = 0; i < s.length(); i++){
-            if(map[s[i]] == 1) return i;
+            if(map[s[i] - 'a'] == 1) return i;
         }
         return -1;
     }
