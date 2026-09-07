efff6a81cef75b1db09c790e7733dcda8b45f82a
Rhea Pawar <89950010+rheapawar@users.noreply.github.com>
2026-09-07 01:21:21 -0400

    Time: 0 ms (100.00%) | Memory: 15.4 MB (37.02%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size()-1;
-      
+        /*binary search similar to min in rotated sorted array and first compare mid to the element that is being searched for if the element is too large then check the left and right bounds, if mid is end 
+        
+        first check which half is sorted rather than checking where the value is initially */
+
+        int left = 0; 
+        int right = nums.size() - 1;
+        int mid;
+
         while(left <= right){
-            int mid = left + (right - left)/2;
+            mid = left + (right - left)/2;
             if(nums[mid] == target) return mid;
             if(nums[left] <= nums[mid]){
-                if(target < nums[mid] && target >= nums[left]) right = mid -1;
+                if(nums[mid] > target && nums[left] <= target) right = mid - 1;
                 else left = mid + 1;
             }
-            else if(nums[mid] <= nums[right]){
-                if(target > nums[mid] && target <= nums[right]) left = mid + 1;
+            else{
+                if(nums[mid] < target && nums[right] >= target) left = mid + 1;
                 else right = mid - 1;
             }
         }
-        return -1;
+        return -1;        
     }
 };
