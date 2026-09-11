7d6e5543ce2c544b1fa18e93d766b1629d3866e9
Rhea Pawar <89950010+rheapawar@users.noreply.github.com>
2026-09-10 22:47:18 -0400

    Time: 7 ms (35.96%) | Memory: 18.5 MB (63.93%) - LeetSync

--- 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.cpp
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     string makeSmallestPalindrome(string s) {
-        int n = s.length();
-        for(int i = 0; i < n/2; i++){
-            (s[i] - 'a' < s[n - i - 1] - 'a') ? s[n - i - 1] = s[i] : s[i] = s[n - i - 1];
+        int right = s.length()-1;
+        int left = 0;
+        while(left < right){
+            s[left] = s[right] = min(s[left], s[right]);
+            left++;
+            right--;
         }
         return s;
-    }
-};
+    }};
