fbb53436622a5dffa79a4d9c3016459cd87c8612
Rhea Pawar <89950010+rheapawar@users.noreply.github.com>
2026-09-14 00:31:01 -0400

    Time: 4 ms (83.94%) | Memory: 11.4 MB (22.70%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -16,15 +16,17 @@ public:
         while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast) break;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
         }
 
-        if(slow != fast) return nullptr;
-        slow = head;
-        while(slow != fast){
-            slow = slow->next;
-            fast = fast->next;
-        }
-        return slow;
+        return nullptr;
+        
     }
 };
