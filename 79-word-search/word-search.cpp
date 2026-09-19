211211279b63e6f87d84e0637c21d2213ee25d1e
Rhea Pawar <89950010+rheapawar@users.noreply.github.com>
2026-09-19 00:03:01 -0400

    Time: 811 ms (24.13%) | Memory: 11.5 MB (43.58%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
         /*initial thought it dfs, iterate over entire board and consider adjacent cells when first letter is discovered*/
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].size(); j++){
+        int m = board.size();
+        int n = board[0].size();
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(board[i][j] == word[0] && helper(board, word, 0, i, j)) return true;
             }
         }
@@ -11,8 +13,11 @@ public:
     }
 
     bool helper(vector<vector<char>>& board, string word, int idx, int i, int j){
+        int m = board.size();
+        int n = board[0].size();
+
         if(idx == word.length()) return true;
-        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[idx]) return false;
+        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx]) return false;
         
         char temp = board[i][j];
         board[i][j] = '#';
