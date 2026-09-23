06bfdbb12c5b724af24eca2618beeceb43d5ee76
Rhea Pawar <89950010+rheapawar@users.noreply.github.com>
2026-09-23 19:09:24 -0400

    Time: 11 ms (11.49%) | Memory: 31.1 MB (19.25%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m = 0;
-        int r = 0;
-        for(int i = 0; i <grid.size(); i++){
+        int area = 0;
+        for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == 1) r = helper(grid, i, j);
-                m = max(r,m);
+                if(grid[i][j] == 1){
+                    area = max(area, dfs(grid, i, j));
+                }
             }
         }
-        return m;
+        return area;
     }
 
-    int helper(vector<vector<int>> &grid, int x, int y){
+    int dfs(vector<vector<int>> &grid, int i, int j){
         int area = 1;
-        grid[x][y] = -1;
-        if(x > 0 && grid[x-1][y] == 1) area += helper(grid, x-1, y);
-        if(x < grid.size() - 1 && grid[x+1][y] == 1) area += helper(grid, x+1, y);
-        if(y > 0 && grid[x][y-1] == 1) area +=helper(grid, x, y-1);
-        if(y < grid[0].size() - 1 && grid[x][y+1] == 1) area += helper(grid, x, y+1);
-        return area;  
+        int m = grid.size();
+        int n = grid[0].size();
+    
+        stack<pair<int, int>> s;
+        grid[i][j] = 0;
+        s.push({i,j});
+
+        while(!s.empty()){
+            auto [x,y] = s.top();
+            s.pop();
+            if(x > 0 && grid[x-1][y] == 1){
+                area++;
+                grid[x-1][y] = 0;
+                s.push({x-1, y});
+            }
+            if(x < m - 1 && grid[x+1][y] == 1){
+                area++;
+                grid[x+1][y] = 0;
+                s.push({x+1, y});
+            }
+            if(y > 0 && grid[x][y-1] == 1){
+                area++;
+                grid[x][y-1] = 0;
+                s.push({x, y-1});
+            }
+            if(y < n - 1 && grid[x][y+1] == 1){
+                area++;
+                grid[x][y+1] = 0;
+                s.push({x, y+1});
+            }
+        }
+        return area;
     }
 };
